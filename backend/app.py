208546620186fbe21b516b9b5be5a3dcf7503457
sanidavidanagama <sanidavidanagama@gmail.com>
2026-02-25 20:32:22 +1100

    feat: configure fastapi backend on app.py in backend and add middleware to the project

---
+++ backend/app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="INKIND API",
+    description="API for INKIND project - DSGP group 15 of Informatics Institute of Technology affiliated with Robert Gordon University, Abeerdeen",
+    version="0.1.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)  

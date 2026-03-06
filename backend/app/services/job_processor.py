4de51a4d653f2ff8e7e1a4f84f1032a538c479c1
sanidavidanagama <sanidavidanagama@gmail.com>
2026-03-06 20:37:47 +0530

    docs: task breakdown for job processing

---
+++ backend/app/services/job_processor.py
@@ -0,0 +1,7 @@
+# TODO: Update job status to "processing"
+# TODO: Run the mood and dia ML pipelines in parallel (using threading or concurency) 
+# TODO: Wait for both results, then call the recommendations model.
+# TODO: Update job status to "done" and save the result in the database.
+
+
+ 
